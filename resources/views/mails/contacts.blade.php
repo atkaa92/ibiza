c15ebb4a8b94617dfef785ddc6a1b0faae87bbdc
Kar3n <atkaa@gmail.com>
2018-02-11 17:21:08 +0400

    mail

--- resources/views/mails/contacts.blade.php
+++ resources/views/mails/contacts.blade.php
@@ -100,7 +100,7 @@
 																	<td align="center" valign="top" class="textContent">
 																		<h1 style="color:#FFFFFF;line-height:100%;font-family:Helvetica,Arial,sans-serif;font-size:35px;font-weight:normal;margin-bottom:5px;text-align:center;">Contact Email</h1>
 																		<h2 style="text-align:center;font-weight:normal;font-family:Helvetica,Arial,sans-serif;font-size:23px;margin-bottom:10px;color:#205478;line-height:135%;">{{ $name }}</h2>
-																		<div style="text-align:center;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;color:#FFFFFF;line-height:135%;">
+																		<div style="text-align:center;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;color:#FFFFFF !important;line-height:135%;">
 																			{{ $email }}
 																		</div>
 																	</td>
