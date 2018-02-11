8e49aa9de04ebb589a03c48643bf127aa0557bbb
Kar3n <atkaa@gmail.com>
2018-02-11 23:20:47 +0400

    asa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Ibiza - Hotel And Sauna</title>
-        <link rel="icon" href="/images/favicon.png" sizes="32x16">
+        <link rel="icon" href="{{ url('images/logo.png') }}" sizes="32x16">
         <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet">
         <link href="{{ url('css/settings.css') }}" rel="stylesheet">
